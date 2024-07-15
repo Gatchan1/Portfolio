35956be7d6fadd2c6675d5f10a53c939a73fc672
Gatchan1 <r.barrio.f@gmail.com>
2024-07-15 17:44:41 +0200

    testing deploy pipeline

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import Seo from '../components/seo';
 const IndexPage = () => {
   return (
     <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <p>Hi! I'm making this by following the Gatsby Tutorial.</p>
       <StaticImage
         alt="Kim playing pinball in his computer; Harry cheers for him."
         src="../images/de pinball (citrus0lemon).jpg"
